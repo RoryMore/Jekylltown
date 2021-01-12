72956d6c3408300811546cf8e3fdf26fb9b188dc
Rory <ruairidh.more@gmail.com>
2021-01-12 15:05:06 +1300

    Updated Camera to add lerp functions

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -9,30 +9,49 @@ public class CameraFollow : MonoBehaviour
 	private Vector3 offset;
 
 
-    private Vector4 currentCam;
-    public Vector4 outsideCam; //X,y,z,x rotation
+
+    private Vector4 currentCam; // The format goes x = x position, y = y position, z = z position and w = x rotation
+    public Vector4 outsideCam;
+    public Vector4 ButcherWindowCam;
 
 	void Start()
 	{
-        SetCam(outsideCam);
-
-        transform.position = new Vector3(currentCam.x, currentCam.y, currentCam.z);
-        transform.localEulerAngles = new Vector3(currentCam.w, 0, 0);
-        //transform.position = new Vector3(target.position.x, target.position.y +6, target.position.z - 4);
-        
-        
-
-		offset = transform.position - target.position;
+        SetCam(outsideCam, false);
 	}
 
 	void FixedUpdate()
 	{
-		Vector3 targetCamPos = target.position + offset;
-		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        FollowPlayer();
 	}
 
-    void SetCam(Vector4 camToCopy)
+    void SetCam(Vector4 camToCopy, bool lerp)
     {
         currentCam = camToCopy;
+
+        //Make it smooth transition
+        if (lerp)
+        {
+            transform.position = Vector3.Lerp(transform.position, currentCam, smoothing * Time.deltaTime);
+            transform.localEulerAngles = new Vector3(currentCam.w, 0, 0);
+            offset = transform.position - target.position;
+
+        }
+
+        //Snap to new location
+        else
+        {
+            transform.position = new Vector3(currentCam.x, currentCam.y, currentCam.z);
+            transform.localEulerAngles = new Vector3(currentCam.w, 0, 0);
+            offset = transform.position - target.position;
+        }
+
+
     }
+
+    void FollowPlayer()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+
 }
