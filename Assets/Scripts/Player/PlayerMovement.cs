ac49a179b90e642004266d03bf894f65bd959a82
Rory <ruairidh.more@gmail.com>
2021-01-12 14:43:21 +1300

    Camera change + Meatbag
    Changed camera angle + system to work purely with WASD movement instead of WASD + Mouse and replaced the meatbag stand in model with the actual one

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,12 +3,15 @@
 public class PlayerMovement : MonoBehaviour
 {
 	public float speed = 6f;
+    public float rotationAnglePerSecond = 4.0f;
 
-	private Vector3 movement;
+
+    private Vector3 movement;
 	private Animator anim;
 	private Rigidbody playerRigidbody;
 	private int floorMask;
 	private float camRayLength = 100f;
+    
 
 	void Awake()
 	{
@@ -22,35 +25,41 @@ public class PlayerMovement : MonoBehaviour
 		float h = Input.GetAxisRaw("Horizontal");
 		float v = Input.GetAxisRaw("Vertical");
 
-		Move(h, v);
-		Turning();
+        Move(h, v);
 		Animating(h, v);
 	}
 
 	void Move(float h, float v)
 	{
-        //movement.Set(h, 0f, v);
+
+        //Move along the horizontal and depth axis
         movement.Set(h, playerRigidbody.velocity.y, v);
 		movement = movement.normalized * speed * Time.deltaTime;
-
 		playerRigidbody.MovePosition(transform.position + movement);
+
+        //New turning system required!
+        if (movement!= Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationAnglePerSecond * Time.deltaTime);
+        }
 	}
 
-	void Turning()
-	{
-		Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit floorHit;
+	//void Turning()
+	//{
+	//	Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+ //       RaycastHit floorHit;
 
 
 
-        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) {
-        	Vector3 playerToMouse = floorHit.point - transform.position;
-        	playerToMouse.y = 0f;
+ //       if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) {
+ //       	Vector3 playerToMouse = floorHit.point - transform.position;
+ //       	playerToMouse.y = 0f;
 
-        	Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
-        	playerRigidbody.MoveRotation(newRotation);
-        }
-    }
+ //       	Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+ //       	playerRigidbody.MoveRotation(newRotation);
+ //       }
+ //   }
 
 	void Animating(float h, float v)
 	{
